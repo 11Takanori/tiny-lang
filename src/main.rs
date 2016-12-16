6ff1240cc1b41b9ed15d530158a0c031b4189835
Takanori Ishibashi <takanori.1112@gmail.com>
2016-12-16 22:05:37 +0900

    Move main to repl

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,7 @@
 pub mod lexer;
 pub mod token;
-
-use std::io::{self, BufRead, Write};
-use token::TokenType;
-use lexer::Lexer;
+pub mod repl;
 
 fn main() {
-    let stdin = io::stdin();
-
-    loop {
-        print!(">> ");
-        io::stdout().flush().expect("Error flushing stdout");
-
-        let mut line = String::new();
-        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
-        let mut lexer = Lexer::new(&mut line);
-
-        loop {
-            let tok = lexer.next_token();
-            println!("{:?}", tok);
-            if tok.token_type == TokenType::EndOfFile {
-                break;
-            }
-        }
-    }
+    repl::start();
 }
---
+++ src/repl.rs
@@ -0,0 +1,27 @@
+// pub mod lexer;
+// pub mod token;
+
+use std::io::{self, BufRead, Write};
+use token::TokenType;
+use lexer::Lexer;
+
+pub fn start() {
+    let stdin = io::stdin();
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
+        let mut lexer = Lexer::new(&mut line);
+
+        loop {
+            let tok = lexer.next_token();
+            println!("{:?}", tok);
+            if tok.token_type == TokenType::EndOfFile {
+                break;
+            }
+        }
+    }
+}
