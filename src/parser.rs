3f35fb3d53d9c30e9b2f2adb850e5a73e5387410
Takanori Ishibashi <takanori.1112@gmail.com>
2016-12-20 07:34:18 +0900

    Add enum

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,28 @@
 use lexer::Lexer;
 
+#[derive(Debug, PartialEq)]
+pub enum ParseType {
+    Lowest,
+    Equals,
+    LessGreater,
+    Sum,
+    Product,
+    Prefix,
+    Call,
+}
+
+
 pub struct Parser<'a> {
     l: Lexer<'a>,
     error: &'a str,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(lexer: Lexer<'a>) -> Parser {
+        let mut parser = Parser {
+            l: lexer,
+            error: "",
+        };
+        return parser;
+    }
 }
