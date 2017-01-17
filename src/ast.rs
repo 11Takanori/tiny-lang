40a35c16955c1e73cf1ddb212db6ddf7d385e985
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-18 07:09:53 +0900

    Fix ast

---
+++ src/ast.rs
@@ -0,0 +1,15 @@
+use token;
+use token::{Token, TokenType};
+
+#[derive(Debug)]
+pub struct Node {
+
+}
+
+#[derive(Debug)]
+pub struct Statement {
+}
+
+#[derive(Debug)]
+pub struct Expression {
+}
