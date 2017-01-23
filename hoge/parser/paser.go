7046d7341c394579a3fa17200ffc363b03824f69
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-24 07:17:09 +0900

    Refactoring

--- hoge/parser/paser.go
+++ hoge/parser/paser.go
@@ -45,10 +45,9 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
-	} else {
-		p.peekError(t)
-		return false
 	}
+	p.peekError(t)
+	return false
 }
 
 func (p *Parser) Errors() []string {
