af5ceed070a2902b17cd266f9d0b88f345d6c797
Takanori Ishibashi <takanori.1112@gmail.com>
2017-01-24 07:47:07 +0900

    Add TestReturnStatements

--- hoge/parser/paser.go
+++ hoge/parser/paser.go
@@ -78,6 +78,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -101,5 +103,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	if !p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
 	return stmt
 }
