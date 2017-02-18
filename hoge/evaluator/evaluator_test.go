b715623e49d049f17bca614d6ddc0ab07fc5070e
Takanori Ishibashi <takanori.1112@gmail.com>
2017-02-18 21:57:31 +0900

    Add TestStringLiteralExpression

--- hoge/evaluator/evaluator_test.go
+++ hoge/evaluator/evaluator_test.go
@@ -291,6 +291,20 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World" {
+		t.Errorf("String has worng value. got=%q", str.Value)
+	}
+}
+
 func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	result, ok := obj.(*object.Boolean)
 	if !ok {
