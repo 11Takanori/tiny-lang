5ea6a42d0e79ccc2c0856530316c7149d06657a5
Takanori Ishibashi <takanori.1112@gmail.com>
2017-02-21 07:45:38 +0900

    Add builtins.go

---
+++ hoge/evaluator/builtins.go
@@ -0,0 +1,10 @@
+package evaluator
+
+import "./../object"
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{Fn: func(args ...object.Object) object.Object {
+		return NULL
+	},
+	},
+}
